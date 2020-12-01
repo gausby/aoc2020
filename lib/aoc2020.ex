bd84f7e2138b555701aee595d41391e229b7c4ab
Martin Gausby <martin.gausby@erlang-solutions.com>
2020-12-01 15:36:06 +0000

    Remove hello world and test

--- lib/aoc2020.ex
+++ lib/aoc2020.ex
@@ -2,17 +2,4 @@ defmodule Aoc2020 do
   @moduledoc """
   Documentation for `Aoc2020`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Aoc2020.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
