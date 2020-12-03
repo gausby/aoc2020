d8cce1edb0161d61703785103ae48c84fc656891
Martin Gausby <martin.gausby@erlang-solutions.com>
2020-12-03 08:44:33 +0000

    Scaffold for day 3

---
+++ test/aoc2020/day_03_test.exs
@@ -0,0 +1,12 @@
+defmodule Aoc2020Test.Day03Test do
+  use ExUnit.Case
+  doctest Aoc2020.Day03
+
+  alias Aoc2020.Day03
+
+  @data_dir Path.join([Application.app_dir(:aoc2020), "priv", "day_03"])
+
+  test "part 1"
+
+  test "part 2"
+end
