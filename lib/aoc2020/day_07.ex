ada4c1c80df2f56121cbc12477d6743a9741c39a
Martin Gausby <martin.gausby@erlang-solutions.com>
2020-12-07 20:38:39 +0000

    A solution to part two of day 7

--- lib/aoc2020/day_07.ex
+++ lib/aoc2020/day_07.ex
@@ -56,7 +56,43 @@ defmodule Aoc2020.Day07 do
 
   ## Part Two
 
-  ...
+  It's getting pretty expensive to fly these days - not because of
+  ticket prices, but because of the ridiculous number of bags you need
+  to buy!
+
+  Consider again your shiny gold bag and the rules from the above
+  example:
+
+  - faded blue bags contain 0 other bags.
+  - dotted black bags contain 0 other bags.
+  - vibrant plum bags contain 11 other bags: 5 faded blue bags and 6
+    dotted black bags.
+  - dark olive bags contain 7 other bags: 3 faded blue bags and 4
+    dotted black bags.
+
+  So, a single shiny gold bag must contain 1 dark olive bag (and the 7
+  bags within it) plus 2 vibrant plum bags (and the 11 bags within
+  each of those): 1 + 1*7 + 2 + 2*11 = 32 bags!
+
+  Of course, the actual rules have a small chance of going several
+  levels deeper than this example; be sure to count all of the bags,
+  even if the nesting becomes topologically impractical!
+
+  Here's another example:
+
+  - shiny gold bags contain 2 dark red bags.
+  - dark red bags contain 2 dark orange bags.
+  - dark orange bags contain 2 dark yellow bags.
+  - dark yellow bags contain 2 dark green bags.
+  - dark green bags contain 2 dark blue bags.
+  - dark blue bags contain 2 dark violet bags.
+  - dark violet bags contain no other bags.
+
+  In this example, a single shiny gold bag must contain 126 other
+  bags.
+
+  How many individual bags are required inside your single shiny gold
+  bag?
   """
 
   defmodule RuleParser.ParserHelpers do
@@ -120,7 +156,6 @@ defmodule Aoc2020.Day07 do
 
   defmodule RuleGraph do
     def from_tokens(tokens) do
-      IO.inspect tokens
       Enum.into(tokens, %{}, fn
         {:ok, [bag: [modifier: mod, color: color], contain: bags], "", _, _, _} ->
           {{mod, color}, bags}
@@ -167,6 +202,18 @@ defmodule Aoc2020.Day07 do
     def fits_in(graph, {_, _} = vertex, acc) do
       neighbours = :digraph.in_neighbours(graph, vertex)
       fits_in(graph, neighbours, acc)
+    end
+
+    @doc """
+    Recursivly count the number of bags required for a given bag type
+    """
+    def total_bags_contained(graph, {_, _} = root_bag) do
+      for edge <- :digraph.out_edges(graph, root_bag),
+          {_, ^root_bag, bag, quantity} = :digraph.edge(graph, edge),
+          reduce: 0 do
+        acc ->
+          acc + quantity + quantity * total_bags_contained(graph, bag)
+      end
     end
   end
 end
--- test/aoc2020/day_07_test.exs
+++ test/aoc2020/day_07_test.exs
@@ -44,5 +44,48 @@ defmodule Aoc2020.Day07Test do
     end
   end
 
-  test "part two"
+  describe "part two" do
+    test "example" do
+      {:ok, graph} =
+        """
+        light red bags contain 1 bright white bag, 2 muted yellow bags.
+        dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+        bright white bags contain 1 shiny gold bag.
+        muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+        shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+        dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+        vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+        faded blue bags contain no other bags.
+        dotted black bags contain no other bags.
+        """
+        |> String.split("\n", trim: true)
+        |> Enum.map(&RuleParser.parse/1)
+        |> RuleGraph.from_tokens()
+
+      assert 32 == RuleGraph.total_bags_contained(graph, {"shiny", "gold"})
+    end
+
+    test "another example" do
+      {:ok, graph} =
+        """
+        shiny gold bags contain 2 dark red bags.
+        dark red bags contain 2 dark orange bags.
+        dark orange bags contain 2 dark yellow bags.
+        dark yellow bags contain 2 dark green bags.
+        dark green bags contain 2 dark blue bags.
+        dark blue bags contain 2 dark violet bags.
+        dark violet bags contain no other bags.
+        """
+        |> String.split("\n", trim: true)
+        |> Enum.map(&RuleParser.parse/1)
+        |> RuleGraph.from_tokens()
+
+      assert 126 == RuleGraph.total_bags_contained(graph, {"shiny", "gold"})
+    end
+
+    test "actual data", %{rule_tokens: rule_tokens} do
+      {:ok, graph} = RuleGraph.from_tokens(rule_tokens)
+      assert 2976 == RuleGraph.total_bags_contained(graph, {"shiny", "gold"})
+    end
+  end
 end
